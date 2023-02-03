7ffc5c299b1bf2d6418e7d24e56ca434d87a0de9
moin105 <moinlatif105@gmail.com>
2023-02-03 13:00:54 +0500

    stela

--- Components/Project/Carousel.js
+++ Components/Project/Carousel.js
@@ -15,42 +15,42 @@ export default class NextJsCarousel extends Component {
 
   render() {
    const  data = [
-    {
+    { "key":"1",
       "name": "Randox | Certifly",
       "description": "Randox is committed to enhancing healthcare through cutting-edge diagnostic technology. We worked on the Randox iOS app, Web app, customer support, IT support, and web portal which allowed us to create certificates for both national and international passengers.",
       "lang": "CSS HTML Firebase PHP LARAVEL",
       "link": "http://covid.randox.com/certifly-app-download/"
-    },{
+    },{"key":"2",
       "name": "ATS-Ticketing",
       "description": "ATS is a Fast, Handy, and Contactless ticket validation & check-in mobile application. ATS is designed especially to resolve the challenge of vehicle ticket scanning with instances of duplicate tickets. ATS's real-time QR Code scanning helps detect fake tickets & speeds up the validation process",
       "lang": "CSS HTML Firebase PHP LARAVEL",
       "link":""
-    },{
+    },{"key":"3",
       "name": "Pet Friendly",
       "description": "Always stay in touch with your pet’s favorite places and discover new places within a few seconds! The goal was to design a mobile application for pet owners so that they can find pet-friendly places near them. Pet owners can now easily find and visit places and can add reviews and images of those places. ",
       "lang": "CSS HTML Firebase PHP LARAVEL",
       "link":""
-    },{
+    },{"key":"4",
       "name": "Writer’s Lock",
       "description": "Writer's Lock is notes writing app. You can use this app to jot down your ideas with ease, make notes and stay focused. The app is customized according to the user's needs and is designed to meet the needs of millions of writers across the globe. Wherever you need to be productive, the Notes app is always nearby.",
       "lang": "CSS HTML Firebase PHP LARAVEL",
       "link":""
-    },{
+    },{"key":"5",
       "name": "WildCatFlashMath",
       "description": "Wildcat Flash Math is a web application designed to quiz students on various Math topics. This app is designed for Valdosta School Systems. Current students must use their account login to access the mobile application. The various math topics include Telling Time, Counting Money, Fractions, Addition, etc",
       "lang": "CSS HTML Firebase PHP LARAVEL",
       "link": "http://wildcatflashmath.com/login.php"
-    },{
+    },{"key":"6",
       "name": "World Enterprises",
       "description": "World Enterprises is providing construction estimating services throughout the US. Their estimators have extensive expertise in estimating for subcontractors, general contractors, estate owners, developers, etc. ",
       "lang": "CSS HTML Firebase PHP LARAVEL",
       "link": "http://worldenterprises.us/"
-    },{
+    },{"key":"7",
       "name": "VoRides",
       "description": "Vorides is a carpooling platform that allows millions of people to get to their destinations safely. Vo rides is a carpooling app designed for the sole purpose of connecting Drivers and Passengers who are going in the same direction, to share both the trip and the associated costs of travel.",
       "lang": "CSS HTML Firebase PHP LARAVEL",
       "link": "http://vorides.com/"
-    },{
+    },{"key":"8",
       "name": "eCareDocta",
       "description": "eCareDocta has come to improve access to healthcare to ensure the African population gets the best possible healthcare from their homes and gets referred to the hospitals when needed. eCareDocta strives to solve the problems of long waiting times at health facilities, by providing automated solutions.",
       "lang": "CSS HTML Firebase PHP LARAVEL",
@@ -115,15 +115,18 @@ export default class NextJsCarousel extends Component {
           }
           showStatus={false}
           showThumbs={false}
-          // autoPlay={true}
+          autoPlay={true}
           interval={3000}
           // autoPlay={true}
           infiniteLoop={true}
           style={{ ...arrowStyles, left: 15 }}
         >
-          {data?.map((child, index) => {
+        
+          {this.props.reverse !== true ? this.props?.data?.map((child, index) => {
+          
             return (
                 <div className={styles.projet} key={index}>
+                  
                 <div className={styles.boxone}>  
              <div className={styles.right}>
                 <Image
@@ -136,16 +139,23 @@ export default class NextJsCarousel extends Component {
                 <p className={styles.fp}>Featured Project</p>
                 <h2 className={styles.h2}>{child.name}</h2>
                 <div className={styles.lightblue}>
-                    <p className={styles.description}>Randox is committed to enhancing healthcare through cutting-edge diagnostic technology. We worked on the Randox iOS app, Web app, customer support, IT support, and web portal which allowed us to create certificates for both national and international passengers.</p>
-                    <p className={styles.lang}>CSS HTML Firebase PHP LARAVEL</p>
+                    <p className={styles.description}>{child.detail}</p>
+                    <p className={styles.lang}>{child.tech}</p>
                 </div>
                 <div className={styles.linkrow}>
-                    <p className={styles.link}>http://covid.randox.com/certifly-app-download/</p>
+                    <p className={styles.link}>{child.link}</p>
                 </div>
               </div>
             
              </div>
-             <div className={styles.boxtwo}>  
+             </div>
+            );
+          }):  this.props?.data?.map((child, index) => {
+          
+            return (
+                <div className={styles.projet} key={index}>
+                  
+                  <div className={styles.boxtwo}>  
              <div className={styles.right}>
                 <Image
                    src={p1}
@@ -155,20 +165,20 @@ export default class NextJsCarousel extends Component {
               </div>
               <div className={styles.left}>
                 <p className={styles.fp}>Featured Project</p>
-                <h2 className={styles.h2}>Randox | Certifly</h2>
+                <h2 className={styles.h2}>{child.name}</h2>
                 <div className={styles.lightblue}>
-                    <p className={styles.description}>Randox is committed to enhancing healthcare through cutting-edge diagnostic technology. We worked on the Randox iOS app, Web app, customer support, IT support, and web portal which allowed us to create certificates for both national and international passengers.</p>
-                    <p className={styles.lang}>CSS HTML Firebase PHP LARAVEL</p>
+                    <p className={styles.description}>{child.detail}</p>
+                    <p className={styles.lang}>{child.tech}</p>
                 </div>
                 <div className={styles.linkrow}>
-                    <p className={styles.link}>http://covid.randox.com/certifly-app-download/</p>
+                    <p className={styles.link}>{child.link}</p>
                 </div>
               </div>
             
              </div>
              </div>
             );
-          })}
+          }) }
         </Carousel>
     );
   }
--- Components/Project/Project.js
+++ Components/Project/Project.js
@@ -4,6 +4,97 @@ import styles from '../../styles/Home.module.css'
 import p1 from '../../public/p1.png'
 import NextJsCarousel from './Carousel'
 function Project() {
+  const cops = [
+    {"key":"",
+        "name":"Randox | Certifly",
+    "detail":"Randox is committed to enhancing healthcare through cutting-edge diagnostic technology. We worked on the Randox iOS app, Web app, customer support, IT support, and web portal which allowed us to create certificates for both national and international passengers.",
+    "tech":"CSS HTML Firebase PHP LARAVEL",
+    "link":"http://covid.randox.com/certifly-app-download/"},
+    {"key":"",
+        "name":"ATS-Ticketing",
+    "detail":"ATS is a Fast, Handy, and Contactless ticket validation & check-in mobile application. ATS is designed especially to resolve the challenge of vehicle ticket scanning with instances of duplicate tickets. ATS's real-time QR Code scanning helps detect fake tickets & speeds up the validation process.",
+    "tech":"CSS HTML Firebase PHP LARAVEL",
+    "link":""},
+    {"key":"",
+    "name":"Pet Friendly",
+    "detail":"Always stay in touch with your pet’s favourite places and discover new places within few seconds!The goal was to design a mobile application for pet owners so that they can find pet friendly places near them. Pet owners can now easily find and visit places, and can add review and images of those places. ",
+    "tech":"CSS HTML Firebase PHP LARAVEL",
+    "link":""},
+    {"key":"",
+    "name":"Writer’s Lock",
+    "detail":"Writer's Lock is notes writing app. You can use this app to jot down your ideas with ease, make notes and stay focused. The app is customized according to the user's needs and is designed to meet the needs of millions of writers across the globe. Wherever you need to be productive, the Notes app is always nearby",
+    "tech":"CSS HTML Firebase PHP LARAVEL",
+    "link":""},
+    {"key":"",
+    "name":"WildCatFlashMath",
+    "detail":"Wildcat Flash Math is a web application designed to quiz students on various Math topics. This app is designed for Valdosta School Systems. Current students must use their account login to access the mobile application. The various math topics include Telling Time, Counting Money, Fractions, Addition etc",
+    "tech":"CSS HTML Firebase PHP LARAVEL",
+    "link":"http://wildcatflashmath.com/login.php"},
+    {"key":"",
+    "name":"World Enterprises",
+    "detail":"World Enterprises is providing construction estimating services throughout US. Their estimators have extensive expertise in estimating for subcontractors, general contractors, estate owners, developers etc.",
+    "tech":"CSS HTML Firebase PHP LARAVEL",
+    "link":"http://worldenterprises.us/"},
+    {"key":"",
+    "name":"VORides",
+    "detail":"Vorides is a carpooling platform that allows millions of people to get to their destinations safely. Vo rides is a carpooling app designed for the sole purpose of connecting Drivers and Passengers who are going in the same direction, to share both the trip and the associated costs of travel.",
+    "tech":"CSS HTML Firebase PHP LARAVEL",
+    "link":"http://vorides.com/"},
+    {"key":"",
+    "name":"eCareDocta",
+    "detail":"eCareDocta has come to improve access to healthcare to ensure the African population gets the best possible healthcare from their homes and gets referred to the hospitals when needed. eCareDocta strives to solve problems of long waiting times at health facilities, by providing automated solution.",
+    "tech":"CSS HTML Firebase PHP LARAVEL",
+    "link":"http://ecareserve.com"}
+    ]
+    const cos = [    
+      {"key":"",
+      "name":"World Enterprises",
+      "detail":"World Enterprises is providing construction estimating services throughout US. Their estimators have extensive expertise in estimating for subcontractors, general contractors, estate owners, developers etc.",
+      "tech":"CSS HTML Firebase PHP LARAVEL",
+      "link":"http://worldenterprises.us/"},
+        {"key":"",
+        "name":"VORides",
+        "detail":"Vorides is a carpooling platform that allows millions of people to get to their destinations safely. Vo rides is a carpooling app designed for the sole purpose of connecting Drivers and Passengers who are going in the same direction, to share both the trip and the associated costs of travel.",
+        "tech":"CSS HTML Firebase PHP LARAVEL",
+        "link":"http://vorides.com/"},
+        {"key":"",
+        "name":"eCareDocta",
+        "detail":"eCareDocta has come to improve access to healthcare to ensure the African population gets the best possible healthcare from their homes and gets referred to the hospitals when needed. eCareDocta strives to solve problems of long waiting times at health facilities, by providing automated solution.",
+        "tech":"CSS HTML Firebase PHP LARAVEL",
+        "link":"http://ecareserve.com"},
+        {"key":"",
+            "name":"Randox | Certifly",
+        "detail":"Randox is committed to enhancing healthcare through cutting-edge diagnostic technology. We worked on the Randox iOS app, Web app, customer support, IT support, and web portal which allowed us to create certificates for both national and international passengers.",
+        "tech":"CSS HTML Firebase PHP LARAVEL",
+        "link":"http://covid.randox.com/certifly-app-download/"},
+      
+      { "key":"",
+    "name":"ATS-Ticketing",
+"detail":"ATS is a Fast, Handy, and Contactless ticket validation & check-in mobile application. ATS is designed especially to resolve the challenge of vehicle ticket scanning with instances of duplicate tickets. ATS's real-time QR Code scanning helps detect fake tickets & speeds up the validation process.",
+"tech":"CSS HTML Firebase PHP LARAVEL",
+"link":""},  {"key":"",
+    "name":"Pet Friendly",
+    "detail":"Always stay in touch with your pet’s favourite places and discover new places within few seconds!The goal was to design a mobile application for pet owners so that they can find pet friendly places near them. Pet owners can now easily find and visit places, and can add review and images of those places. ",
+    "tech":"CSS HTML Firebase PHP LARAVEL",
+    "link":""},
+      {"key":"",
+    "name":"Writer’s Lock",
+    "detail":"Writer's Lock is notes writing app. You can use this app to jot down your ideas with ease, make notes and stay focused. The app is customized according to the user's needs and is designed to meet the needs of millions of writers across the globe. Wherever you need to be productive, the Notes app is always nearby",
+    "tech":"CSS HTML Firebase PHP LARAVEL",
+    "link":""},
+       {"key":"",
+    "name":"WildCatFlashMath",
+    "detail":"Wildcat Flash Math is a web application designed to quiz students on various Math topics. This app is designed for Valdosta School Systems. Current students must use their account login to access the mobile application. The various math topics include Telling Time, Counting Money, Fractions, Addition etc",
+    "tech":"CSS HTML Firebase PHP LARAVEL",
+    "link":"http://wildcatflashmath.com/login.php"}, 
+  
+     
+   
+     
+    
+   
+   
+      ]
   return (
     <>
         <div className={styles.project}>
@@ -15,7 +106,8 @@ function Project() {
              </div>
              </div>
              <div className={styles.carou}>
-              <NextJsCarousel/>
+              <NextJsCarousel data={cops} reverse={false}/>
+              <NextJsCarousel data={cos} reverse={true}/>
              </div>
              
              {/* <div className={styles.boxone}>  
